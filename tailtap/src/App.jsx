dfdbc9ae324479d5d754d866b88051397d6c1ff5
mcneilr2 <robyn55555@gmail.com>
2024-11-20 11:09:26 -0800

    React + Vite set up, extra boilerplate removed

---
+++ tailtap/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>
+        Welcome to TailTap Front-End
+      </p>
+    </>
+  )
+}
+
+export default App
