aeca23595e24338c64d1042329584227e7844e28
mcneilr2 <robyn55555@gmail.com>
2024-11-28 19:39:34 -0800

    video and header styling improved, animation added to header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,18 +1,35 @@
+import skateboard from "../../src/assets/images/button.svg";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "../../src/assets/images/logo.svg";
+import logo from "../../src/assets/images/tailtap-logo.svg"; // Static logo (or first frame of the GIF)
+import animatedLogo from "../../src/assets/images/gif.gif"; // Animated GIF for hover
+import skateboardGraphic from "../../src/assets/images/button.svg";
 import "./Header.scss";
 
 export default function Header() {
+  const [isHovered, setIsHovered] = useState(false); // Track hover state
+
   return (
     <header className="header">
-      <Link to="/" className="header__link-logo">
-        <img className="header__logo" src={logo} alt="tailtap logo" />
+      <Link to="/" className="header_link-logo">
+        <img
+          className="header__logo"
+          src={isHovered ? animatedLogo : logo} // Swap between GIF and static logo
+          alt="tailtap logo"
+          onMouseEnter={() => setIsHovered(true)} // Trigger hover
+          onMouseLeave={() => setIsHovered(false)} // Reset hover
+        />
       </Link>
       <div className="header__vertical-container">
         <h1 className="header__title">TAILTAP</h1>
         <h3 className="header__caption">SKATE + LEARN + SHARE</h3>
+        <button
+          style={{ backgroundImage: `url(${skateboardGraphic})` }}
+          className="header__button"
+        >
+          Spot Finder
+        </button>
       </div>
-      <button className="header__button">Find Skate Spot!</button>
     </header>
   );
 }
--- pages/SpotsPage/SpotsPage.jsx
+++ pages/SpotsPage/SpotsPage.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import YouTube from "react-youtube";
 import axios from "axios";
 import "./SpotsPage.scss";
+import ramp from "../../src/assets/images/ramp.svg";
+import rail from "../../src/assets/images/rail.svg";
 
 export default function SpotsPage() {
   const params = useParams();
@@ -28,5 +29,34 @@ export default function SpotsPage() {
     getSpot();
   }, [selectedSpotId]);
 
-  return <>{/* <YouTube videoId={activeSpot.homepage_video_id} /> */}</>;
+  return (
+    <>
+      <h2 className="spot__details-header">{activeSpot.name}</h2>
+      <div className="hero__video">
+        <iframe
+          frameborder="0"
+          allowfullscreen=""
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          title="Session Atlas - BC - Vancouver Plaza Skate Park"
+          src="https://www.youtube.com/embed/gPpJ2lcCOxI?enablejsapi=1&amp;origin=http%3A%2F%2Flocalhost%3A5173&amp;widgetid=3"
+          id="widget4"
+        ></iframe>
+      </div>
+      <article className="spot__details">
+        <div className="spot__detail-container">
+          <label className="spot__detail-label">Spot Type:</label>
+          <h2 className="spot__detail-text">{activeSpot.type}</h2>
+          <img
+            src={activeSpot.type == "Park" ? ramp : rail}
+            alt="spot type graphic"
+          />
+        </div>
+        <div className="spot__detail-container">
+          <label className="spot__detail-label">Spot Type:</label>
+          <h2 className="spot__detail-text">{activeSpot.type}</h2>
+        </div>
+      </article>
+    </>
+  );
 }
