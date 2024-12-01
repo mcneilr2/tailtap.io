0644d019536b261863bc480411c225c2d094cde5
mcneilr2 <robyn55555@gmail.com>
2024-11-30 16:02:17 -0800

    spot page mobile design complete

--- pages/SpotsPage/SpotsPage.jsx
+++ pages/SpotsPage/SpotsPage.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import GaugeComponent from "react-gauge-component";
 import axios from "axios";
 import "./SpotsPage.scss";
 import ramp from "../../src/assets/images/ramp.svg";
 import rail from "../../src/assets/images/rail.svg";
 import lit from "../../src/assets/images/lit.svg";
 import notLit from "../../src/assets/images/not-lit.svg";
+import wet from "../../src/assets/images/wet_floor.svg";
+import swim from "../../src/assets/images/swim.svg";
+import emerg from "../../src/assets/images/emerg.svg";
 
 export default function SpotsPage() {
   const params = useParams();
@@ -41,45 +45,131 @@ export default function SpotsPage() {
           <iframe
             frameborder="0"
             allowfullscreen=""
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
-            title="Session Atlas - BC - Vancouver Plaza Skate Park"
-            src="https://www.youtube.com/embed/gPpJ2lcCOxI?enablejsapi=1&amp;origin=http%3A%2F%2Flocalhost%3A5173&amp;widgetid=3"
+            src={`https://www.youtube.com/embed/${activeSpot.homepage_video_id}?enablejsapi=1&amp;origin=http%3A%2F%2Flocalhost%3A5173&amp;widgetid=3`}
             id="widget4"
           ></iframe>
         </div>
         <section className="spot__details">
-          <div className="spot__detail-container--row">
-            <div className="spot__detail-container">
-              <h3 className="spot__detail-label">Spot Type</h3>
-              <p className="spot__detail-text">{activeSpot.type}</p>
+          <div className="spot__detail-container">
+            <h3 className="spot__detail-label">Spot Type</h3>
+            <p className="spot__detail-text--inline">{activeSpot.type}</p>
+            <img
+              className="spot__detail-icon"
+              src={activeSpot.type == "Park" ? ramp : rail}
+              alt="spot type graphic"
+            />
+          </div>
+          <div className="spot__detail-container">
+            <h3 className="spot__detail-label">Activity Level</h3>
+            <GaugeComponent
+              className="spot__detail-icon--gauge"
+              arc={{
+                subArcs: [
+                  {
+                    limit: 20,
+                    color: "#5BE12C",
+                    showTick: true,
+                  },
+                  {
+                    limit: 40,
+                    color: "#F5CD19",
+                    showTick: true,
+                  },
+                  {
+                    limit: 60,
+                    color: "#F58B19",
+                    showTick: true,
+                  },
+                  {
+                    limit: 80,
+                    color: "#EA4228",
+                    showTick: true,
+                  },
+                  {
+                    limit: 100,
+                    color: "#EA4228",
+                    showTick: true,
+                  },
+                ],
+              }}
+              value={activeSpot.occupancy_level * 10}
+            />
+          </div>
+          <div className="spot__detail-container">
+            <h3 className="spot__detail-label">Median Skill Level</h3>
+            <GaugeComponent
+              className="spot__detail-icon--gauge"
+              arc={{
+                subArcs: [
+                  {
+                    limit: 20,
+                    color: "#5BE12C",
+                    showTick: true,
+                  },
+                  {
+                    limit: 40,
+                    color: "#F5CD19",
+                    showTick: true,
+                  },
+                  {
+                    limit: 60,
+                    color: "#F58B19",
+                    showTick: true,
+                  },
+                  {
+                    limit: 80,
+                    color: "#EA4228",
+                    showTick: true,
+                  },
+                  {
+                    limit: 100,
+                    color: "#EA4228",
+                    showTick: true,
+                  },
+                ],
+              }}
+              value={activeSpot.median_skill_level * 10}
+            />
+          </div>
+          <div className="spot__detail-container">
+            <h3 className="spot__detail-label">Access</h3>
+            <div className="spot__detail-container--row">
+              <label className="spot__detail-text-label">Open Hours:</label>
+              <p className="spot__detail-text">{activeSpot.hours}</p>
+            </div>
+            <div className="spot__detail-container--row">
+              <label className="spot__detail-text-label">Cost to Skate:</label>
+              <p className="spot__detail-text">
+                {activeSpot.is_public ? "Free!" : "Private"}
+              </p>
+            </div>
+            <div className="spot__detail-container--row">
+              <label className="spot__detail-text-label">Night Lighting:</label>
+              <p className="spot__detail-text">
+                {activeSpot.is_lit_night ? "It's Lit!" : "No Lights"}
+              </p>
+
               <img
-                className="spot__detail-icon"
-                src={activeSpot.type == "Park" ? ramp : rail}
-                alt="spot type graphic"
+                className="spot__detail-icon--small"
+                src={activeSpot.is_lit_night ? lit : notLit}
+                alt=""
               />
             </div>
-            <div className="spot__detail-container">
-              <h3 className="spot__detail-label">Access</h3>
-              <div className="spot__detail-container--row">
-                <label className="spot__detail-text-label">Open Hours:</label>
-                <p className="spot__detail-text">{activeSpot.hours}</p>
-              </div>
-              <div className="spot__detail-container--row">
-                <label className="spot__detail-text-label">
-                  Night Lighting:
-                </label>
-                {activeSpot.is_lit_night ? "It's Lit!" : "No Lights"}
-                <img
-                  className="spot__detail-icon--small"
-                  src={activeSpot.is_lit_night ? lit : notLit}
-                  alt=""
-                />
-              </div>
+            <div className="spot__detail-container--row">
+              <label className="spot__detail-text-label">Rain Coverage:</label>
+              <p className="spot__detail-text">
+                {activeSpot.weather_coverage == "partial" ? "Partial" : "None"}
+              </p>
+              <img
+                className="spot__detail-icon--small"
+                src={activeSpot.weather_coverage == "partial" ? wet : swim}
+                alt=""
+              />
             </div>
           </div>
           <div className="spot__detail-container--vertical">
-            <h3 className="spot__detail-label">Location</h3>
+            <h3 className="spot__detail-label">Map</h3>
             <iframe
               className="spot__detail-map"
               loading="lazy"
@@ -87,6 +177,28 @@ export default function SpotsPage() {
               referrerpolicy="no-referrer-when-downgrade"
               src={`https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${activeSpot.latitude},${activeSpot.longitude}`}
             ></iframe>
+          </div>
+          <div className="spot__detail-container--row">
+            <h3 className="spot__detail-label">Address: </h3>
+            <p className="spot__detail-text--inline">{activeSpot.address}</p>
+          </div>
+          <div className="spot__detail-container--row-emerg">
+            <img
+              className="spot__detail-icon--emerg"
+              src={emerg}
+              alt="emergency logo"
+            />
+            <div className="spot__detail-container--emerg">
+              <h3 className="spot__detail-label--emerg">Closest Hospital </h3>
+              <label className="spot__detail__text-abel">Name:</label>
+              <p className="spot__detail-text--inline-emerg">
+                {activeSpot.closest_hospital_name}
+              </p>
+              <label className="spot__detail__text-abel">Address:</label>
+              <p className="spot__detail-text--inline-emerg">
+                {activeSpot.closest_hospital_address}
+              </p>
+            </div>
           </div>
         </section>
       </article>
